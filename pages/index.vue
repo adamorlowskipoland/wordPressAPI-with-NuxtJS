e6c475a3256bdf9b60ed077dd7896a7cb3a3321c
Adam <adamorlowskipoland@icloud.com>
2018-05-12 11:36:23 +0200

    displaying posts from local wordpress

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="container">
+    <div>
+      <PostList :posts="posts"/>
+    </div>
+  </section>
+</template>
+
+<script>
+  import PostList from '~/components/posts/PostList.vue'
+  import wp from '~/lib/wp'
+
+  export default {
+    async asyncData ({params}) {
+      return wp.posts()
+    },
+    components: {
+      PostList
+    }
+  }
+</script>
+
+<style>
+  .container {
+    margin: 1rem auto;
+    width: 90%;
+    max-width: 900px;
+  }
+</style>
