0e566af1d8702cf47e1deba48cd6578da9947994
Adam <adamorlowskipoland@icloud.com>
2018-05-12 18:18:42 +0200

    add data to store

--- pages/index.vue
+++ pages/index.vue
@@ -11,19 +11,16 @@
   import wp from '~/lib/wp'
 
   export default {
-    async asyncData ({params}) {
+    async asyncData ({ params }) {
       return wp.posts()
+    },
+    head () {
+      return {
+        title: `${this.$store.state.site_data.name} | Home`
+      }
     },
     components: {
       PostList
     }
   }
 </script>
-
-<style>
-  .container {
-    margin: 1rem auto;
-    width: 90%;
-    max-width: 900px;
-  }
-</style>
